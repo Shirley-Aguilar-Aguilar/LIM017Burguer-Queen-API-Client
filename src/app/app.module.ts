0a2bab4e99ede0a9decbf309747e61b38910df32
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-06 20:51:04 -0500

    añadiendo importaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { HeadernavComponent } from './component/headernav/headernav.component';
+import { CreateUsersComponent } from './component/body/users/create-users/create-users.component';
+import { ViewUsersComponent } from './component/body/users/view-users/view-users.component';
+import { ViewProductsComponent } from './component/body/products/view-products/view-products.component';
+import { CreateOrdersComponent } from './component/body/orders/create-orders/create-orders.component';
+import { OrdersPendingComponent } from './component/body/orders/view-orders/orders-pending/orders-pending.component';
+import { OrdersStatusComponent } from './component/body/orders/view-orders/orders-status/orders-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeadernavComponent,
+    CreateUsersComponent,
+    ViewUsersComponent,
+    ViewProductsComponent,
+    CreateOrdersComponent,
+    OrdersPendingComponent,
+    OrdersStatusComponent
   ],
   imports: [
     BrowserModule
