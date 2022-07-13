8fa2d0086a960271870273b4bf1e4d6c19d3a584
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-13 13:37:32 -0500

    funcionalidad de navvar

--- src/app/component/headernav/headernav.component.ts
+++ src/app/component/headernav/headernav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-headernav',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./headernav.component.css']
 })
 export class HeadernavComponent implements OnInit {
-
+  opened = true;
+  name:string|null= sessionStorage.getItem('name');
+  @ViewChild('sidenav', { static: true })
+  sidenav!: MatSidenav;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(window.innerWidth)
+    if (window.innerWidth < 768) {
+      this.sidenav.fixedTopGap = 55;
+      this.opened = false;
+    } else {
+      this.sidenav.fixedTopGap = 55;
+      this.opened = true;
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: number; }; }) {
+    if (event.target.innerWidth < 768) {
+      this.sidenav.fixedTopGap = 55;
+      this.opened = false;
+    } else {
+      this.sidenav.fixedTopGap = 55
+      this.opened = true;
+    }
+  }
+
+  isBiggerScreen() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (width < 768) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
