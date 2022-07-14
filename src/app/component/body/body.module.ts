fa4b6230d4518d5dd1762782c0946f43649a9f8e
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-14 14:13:24 -0500

    creando dialogos para crud de users

--- src/app/component/body/body.module.ts
+++ src/app/component/body/body.module.ts
@@ -3,17 +3,18 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatDialogModule } from "@angular/material/dialog";
 import { AngularMaterialModule } from "src/app/angular-material/angular-material.module";
 import { BodyRoutingModule } from "./body-routing.module";
+import { DialogCorrect, DialogData, DialogIncorrect, DialogUpdateWorker } from "./dialogs/dialogsTs/dialogs.component";
 import { CreateOrdersComponent } from "./orders/create-orders/create-orders.component";
 import { OrdersPendingComponent } from "./orders/view-orders/orders-pending/orders-pending.component";
 import { OrdersStatusComponent } from "./orders/view-orders/orders-status/orders-status.component";
 import { ViewProductsComponent } from "./products/view-products/view-products.component";
 import { CreateUsersComponent } from "./users/create-users/create-users.component";
-import { DialogCorrect, DialogData, DialogIncorrect, ViewUsersComponent } from "./users/view-users/view-users.component";
+import { ViewUsersComponent } from "./users/view-users/view-users.component";
 
 @NgModule ({
   declarations: [
     CreateUsersComponent,
-    ViewUsersComponent,
+    ViewUsersComponent ,
     ViewProductsComponent,
     CreateOrdersComponent,
     OrdersPendingComponent,
@@ -21,6 +22,7 @@ import { DialogCorrect, DialogData, DialogIncorrect, ViewUsersComponent } from "
     DialogData,
     DialogIncorrect,
     DialogCorrect,
+    DialogUpdateWorker,
   ],
   imports: [
    BodyRoutingModule,
--- src/app/component/body/users/view-users/view-users.component.ts
+++ src/app/component/body/users/view-users/view-users.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import { RestService } from 'src/app/services/rest.service';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCorrect, DialogData, DialogIncorrect, DialogUpdateWorker } from '../../dialogs/dialogsTs/dialogs.component';
 
-export interface DialogData {
-  delete: boolean;
-}
+
 @Component({
   selector: 'app-view-users',
   templateUrl: './view-users.component.html',
@@ -19,7 +17,6 @@ export class ViewUsersComponent implements OnInit {
 
   constructor(
     private RestService:RestService,
-    private formBuilder: FormBuilder,
     public dialog: MatDialog
     ) {  }
 
@@ -34,21 +31,8 @@ export class ViewUsersComponent implements OnInit {
 
   // conexión con clases para mostrar dialogos
   openDialogCorrect(): void {const dialogRef = this.dialog.open(DialogCorrect , {});}
-  openDialogIncorrect(): void {const dialogRef = this.dialog.open(DialogIncorrect , {});}
+  openDialogIncorrect(): void {const dialogRef = this.dialog.open(DialogIncorrect, {});}
 
-
-  // actualizando usuario
-  public updateUser(element:any){
-   console.log(element.id);
-   console.log(element);
-  }
-  public updateWorker(id: number, data:any){
-    this.RestService.put('users', data,id)
-    .subscribe({
-      next: data => {this.openDialogCorrect();},
-      error: error => {this.openDialogIncorrect();}
-    })
-  }
 
   // dialogos al usar el verbo delete
   openDialog(id:number): void {
@@ -57,6 +41,7 @@ export class ViewUsersComponent implements OnInit {
       if(result){this.deleteWorker(id);}
     });
   }
+
 
   // eliminando usuario
   public deleteWorker(id: number){
@@ -68,104 +53,30 @@ export class ViewUsersComponent implements OnInit {
   }
 
 
-
-}
-
-// creación de templates de dialogos
-@Component({
-  selector: 'dialog',
-  templateUrl: './dialog.component.html',
-  styles: [`
-
-    :host {
-      border:0;
-      font-size:20px;
-      text-align:center;
-      border-radius: 8px;
-      font-family: 'Patua One', cursive;
-      display:flex;
-      flex-direction:column;
-      align-items:center;
-      justify-content:center;
-      box-shadow:3px 3px 3px gray;
-      padding:5%;
-      font-family: 'Patua One', cursive;
+  openDialogUpdate(element:any): void {
+    const dialogRef = this.dialog.open(DialogUpdateWorker , {
+      data: {email : element.email, password : element.password, admin : element.admin, userRol : element.userrol.id,}
     }
-    h1{ font-family: 'Patua One', cursive;font-size:20px;}
-    button{
-      background-color:black;
-      color:white;
-      font-size:15px;
-    }
-    button:hover{background-color:red;}
-  `]
-})
-export class DialogData {
-  constructor( public dialogRef: MatDialogRef<DialogData>,) {}
-  onNoClick(): void {this.dialogRef.close();}
-}
 
-@Component({
-  selector: 'dialog-correct',
-  templateUrl: './dialogcorrect.component.html',
-  styles: [`
-    :host {
-      display: block;
-      border:0;
-      font-size:20px;
-      text-align:center;
-      border-radius: 8px;
-      font-family: 'Patua One', cursive;
-      display:flex;
-      flex-direction:column;
-      align-items:center;
-      justify-content:center;
-      padding:5%;
-    }
-    button {
-      background-color:black;
-      color:white;
-      font-size:15px;
-    }
-    button:hover{background-color:red;}
-  `]
-})
-export class DialogCorrect {
-  constructor( public dialogRef: MatDialogRef<DialogCorrect>,) {}
-  onNoClick(): void {
-    this.dialogRef.close();
-    window.location.reload();
+      );
+    dialogRef.afterClosed().subscribe(result => {
+      //if(result){this.deleteWorker(id);}
+    });
   }
-}
+  // actualizando usuario
+  public updateUser(element:any){
+    console.log(element.id);
+    console.log(element);
+   }
+   public updateWorker(id: number, data:any){
+     this.RestService.put('users', data,id)
+     .subscribe({
+       next: data => {this.openDialogCorrect();},
+       error: error => {this.openDialogIncorrect();}
+     })
+   }
 
-@Component({
-  selector: 'dialog-incorrect',
-  templateUrl: './dialogincorrect.component.html',
-  styles: [`
-    :host {
-      display: block;
-      border:0;
-      font-size:20px;
-      text-align:center;
-      border-radius: 8px;
-      font-family: 'Patua One', cursive;
-      display:flex;
-      flex-direction:column;
-      align-items:center;
-      justify-content:center;
-    }
-    button{
-      background-color:black;
-      color:white;
-      font-size:15px;
-    }
-    button:hover{background-color:red;}
-  `]
-})
-export class DialogIncorrect {
-  constructor( public dialogRef : MatDialogRef<DialogIncorrect>,) {}
-  onNoClick(): void {this.dialogRef.close();}
-}
 
+}
 
 
