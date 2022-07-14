b616d77d515bc7e278a6a136bdbc5cae24290497
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-14 14:10:19 -0500

    mejorando ux segun feedback

--- src/app/component/body/users/create-users/create-users.component.ts
+++ src/app/component/body/users/create-users/create-users.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { RestService } from 'src/app/services/rest.service';
+import { DialogCorrect, DialogIncorrect } from '../../dialogs/dialogsTs/dialogs.component';
 
 interface Rol {
   value: number;
   viewValue: string;
 }
 
+export interface DialogData {
+  delete: boolean;
+}
 @Component({
   selector: 'app-create-users',
   templateUrl: './create-users.component.html',
@@ -17,7 +22,8 @@ export class CreateUsersComponent implements OnInit {
 
   constructor(
     private RestService: RestService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public dialog: MatDialog,
   ) { this.buildForm(); }
 
   ngOnInit(): void {
@@ -36,49 +42,53 @@ export class CreateUsersComponent implements OnInit {
       name:['', [Validators.required]],
       email: ['', [Validators.required, Validators.email ] ],
       password: ['', [Validators.required, Validators.minLength(6) ] ],
-      admin:['',[Validators.required]],
+      admin:['',],
       userRolId:['',[Validators.required]]
     });
     this.formCreateUser.valid;
   }
 
-
+  // validación de inputs
+  get emailField(){ return this.formCreateUser.get('email') }
+  get passwordField(){ return this.formCreateUser.get('password') }
+  get nameField(){ return this.formCreateUser.get('name') }
 
   save(event:Event) {
     event.preventDefault();
     if(this.formCreateUser.valid){
+      if(this.formCreateUser.value.userRolId === 1){
+        this.formCreateUser.value.admin = true
+      }else {this.formCreateUser.value.admin = false};
       console.log("this.formCreateUser");
       console.log(this.formCreateUser.value);
-
-      // this.createUser(this.formCreateUser.value)
+      this.createUser(this.formCreateUser.value)
     } else {
       this.formCreateUser.markAllAsTouched();
     }
   }
 
-   // validación de inputs
-   get emailField(){ return this.formCreateUser.get('email') }
-   get passwordField(){ return this.formCreateUser.get('password') }
-   get nameField(){ return this.formCreateUser.get('name') }
 
-   public clear(){
-    this.formCreateUser.reset()
-   }
+   public clear(){this.formCreateUser.reset()}
 
+    // conexión con clases para mostrar dialogos
+    openDialogCorrect(): void {const dialogRef = this.dialog.open(DialogCorrect , {});}
+    openDialogIncorrect(): void {const dialogRef = this.dialog.open(DialogIncorrect , {});}
 
-/*   public createUser(data:any){
-    this.RestService.postUser(data)
+  public createUser(dataUser:any){
+    this.RestService.post('users',dataUser)
     .subscribe({
       next: data => {
         console.log("datapostuser");
          console.log(data);
+         this.openDialogCorrect();
          this.formCreateUser.reset();
       },
       error: error => {
         this.error = true;
-        console.log(JSON.stringify(error.error.message));
+        console.log(JSON.stringify(error.error));
+        this.openDialogIncorrect()
       }
     })
-  } */
+  }
 
 }
