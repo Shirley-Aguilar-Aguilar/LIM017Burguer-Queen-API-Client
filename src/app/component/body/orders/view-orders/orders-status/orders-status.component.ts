e883021084527f99982006c9b1899ef23f5482d4
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-08-12 10:35:24 -0500

    estructura, funcionalidad y style para order-status

--- src/app/component/body/orders/view-orders/orders-status/orders-status.component.ts
+++ src/app/component/body/orders/view-orders/orders-status/orders-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-orders-status',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersStatusComponent implements OnInit {
 
-  constructor() { }
+ orders:any;
+ totalPrice:number=0;
+  constructor(public RestService: RestService) { }
 
   ngOnInit(): void {
+    this.getStatusOrder();
+  }
+
+  getPriceTotal(data:any){
+    data.map((order:any) => {
+      let sum = 0;
+
+      const pricePerProduct =  order.ordersproducts.map((product:any) => {
+          return product.quantity*product.product.price;
+      })
+
+      for (let i = 0; i < pricePerProduct.length; i++) {
+          sum += pricePerProduct[i];
+      }
+      order.newTotal = sum;
+
+    })
+  }
+
+  public getStatusOrder(){
+    this.RestService.get('getAllOrders')
+    .subscribe({
+      next: data => {
+        console.log(data);
+        const priceTotal =this.getPriceTotal(data);
+        this.orders = data;
+      },
+      error: error => {
+        console.log(error)
+      }
+    })
+  }
+
+  addPrice(){
+    console.log("this.", this.totalPrice)
   }
 
 }
