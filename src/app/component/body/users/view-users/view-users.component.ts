0ea4327f2e947ca595a569d455df72d9eb1a809b
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-08-12 19:09:06 -0500

    agregando paginación y filtrado de datos de tabla users

--- src/app/component/body/users/view-users/view-users.component.ts
+++ src/app/component/body/users/view-users/view-users.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCorrect, DialogData, DialogIncorrect } from '../../dialogs/dialogsTs/dialogs.component';
 import { DialogUpdateWorker } from '../../dialogs/updateDialogsWorker/dialogUpdateWorker.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 
 @Component({
@@ -11,8 +14,11 @@ import { DialogUpdateWorker } from '../../dialogs/updateDialogsWorker/dialogUpda
   styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent implements OnInit {
-  public listWorkers:any = [];
-  dataSource = this.listWorkers;
+/*   public listWorkers:any = [];
+  dataSource = this.listWorkers; */
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   displayedColumns: string[] = ['id', 'name','email', 'rol','update', 'delete'];
   error: boolean = false;
 
@@ -28,7 +34,20 @@ export class ViewUsersComponent implements OnInit {
 
   public getWorkers() {
     this.RestService.get('users')
-    .subscribe((response) =>  this.dataSource = response);
+    .subscribe((data) =>  {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   // conexión con clases para mostrar dialogos
